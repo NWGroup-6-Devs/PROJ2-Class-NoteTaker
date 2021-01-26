2f960aab2a30c7b7bbe222a85e9ac852676efc73
El Rafa <rafaelrosas19@gmail.com>
2021-01-26 14:55:44 -0600

    Merging our working files with the passport boilerplate

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require("sequelize");
-module.exports = new Sequelize("wendi", "root", "@Rr62508874!", {
-  host: "localhost",
-  dialect: "mysql",
-  define: {
-    timestamps: false,
-  },
-});
